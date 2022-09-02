ebafa44e794b40c3e355c79fc389d2ba56c04298
saadahmed2452 <112680021+saadahmed2452@users.noreply.github.com>
2022-09-02 10:56:33 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$dbHost = 'localhost';
-$dbName = 'test';
-$dbUsername = 'root';
-$dbPassword = 'root';
-$dbc= mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-?>
- 
